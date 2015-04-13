26d1034627391c485d4288969e47696337425a25
syouon <syouon.do@gmail.com>
2015-04-13 18:00:21 +0200

    Main added

---
+++ computer-database/src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import services.Services;
+import ui.AddComputerChoice;
+import ui.DeleteComputerChoice;
+import ui.ListCompaniesChoice;
+import ui.ListComputersChoice;
+import ui.Menu;
+import ui.QuitChoice;
+import ui.ShowDetailsChoice;
+import ui.UpdateDiscontinuationChoice;
+import ui.UpdateIntroductionChoice;
+import dbconnection.DatabaseConnection;
+import dbconnection.MySQLConnection;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DatabaseConnection conn = MySQLConnection.getInstance();
+		Services services = conn.connect();
+
+		if (services == null) {
+			System.out.println("Connection failed!");
+			return;
+		}
+
+		Menu menu = new Menu();
+		menu.addChoice(new QuitChoice());
+		menu.addChoice(new ListComputersChoice(services));
+		menu.addChoice(new ListCompaniesChoice(services));
+		menu.addChoice(new ShowDetailsChoice(services));
+		menu.addChoice(new AddComputerChoice(services));
+		menu.addChoice(new UpdateIntroductionChoice(services));
+		menu.addChoice(new UpdateDiscontinuationChoice(services));
+		menu.addChoice(new DeleteComputerChoice(services));
+		menu.loop();
+
+		conn.deconnect();
+	}
+
+}
