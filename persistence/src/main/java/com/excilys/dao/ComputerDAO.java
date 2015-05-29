a0328df4264ca232400c7385d5b596c0a25a13ed
syouon <syouon.do@gmail.com>
2015-05-29 10:49:03 +0200

    Search by company name added in persistence module

--- persistence/src/main/java/com/excilys/dao/ComputerDAO.java
+++ persistence/src/main/java/com/excilys/dao/ComputerDAO.java
@@ -4,16 +4,19 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.excilys.model.Computer;
 
 public interface ComputerDAO extends JpaRepository<Computer, Long> {
 
-	// TODO count by name OR company name
-	public int countByNameContaining(String name);
+	@Query("select count(c) from Computer c left join c.company co where c.name like %:name% or co.name like %:name%")
+	public int countByNameContaining(@Param("name") String name);
 
 	public void deleteByCompanyId(long companyId);
 
-	// TODO research by name OR company name
-	public List<Computer> findByNameContaining(String regex, Pageable page);
+	@Query("select c from Computer c left join c.company co where c.name like %:name% or co.name like %:name%")
+	public List<Computer> findByNameContaining(@Param("name") String regex,
+			Pageable page);
 }
